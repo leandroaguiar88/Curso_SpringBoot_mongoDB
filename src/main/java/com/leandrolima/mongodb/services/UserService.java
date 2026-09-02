5904c2891d79717bd9b852ac4ab9a2c9a8053a2c
unknown <leoaguiarlima@gmail.com>
2026-09-02 10:39:45 -0300

    user update

--- src/main/java/com/leandrolima/mongodb/services/UserService.java
+++ src/main/java/com/leandrolima/mongodb/services/UserService.java
@@ -28,6 +28,19 @@ public class UserService {
 		return repo.insert(obj);
 
 	}
+	public User update(User obj) {
+	    User newObj = repo.findById(obj.getId())
+	        .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	    
+	    updateData(newObj, obj);
+	    return repo.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+		
+	}
 
 	public void delete(String id) {
 		findById(id);
