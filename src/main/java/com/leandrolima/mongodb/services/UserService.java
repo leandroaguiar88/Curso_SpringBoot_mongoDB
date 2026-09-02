5a6502679a27989823e77bf14a9f8ff04c06c8ed
unknown <leoaguiarlima@gmail.com>
2026-09-02 09:12:59 -0300

    User insertion through POST

--- src/main/java/com/leandrolima/mongodb/services/UserService.java
+++ src/main/java/com/leandrolima/mongodb/services/UserService.java
@@ -6,24 +6,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.leandrolima.mongodb.domain.User;
+import com.leandrolima.mongodb.dto.UserDTO;
 import com.leandrolima.mongodb.repository.UserRepository;
 import com.leandrolima.mongodb.services.exception.ObjectNotFoundException;
-
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
-	public User findById(String id) { 
-	    return repo.findById(id)
-	        .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
-	}
+
+	public User findById(String id) {
+		return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
+	public User insert(User obj) {
+		return repo.insert(obj);
 
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(),objDto.getEmail());
+		
+	}
+		
+}
