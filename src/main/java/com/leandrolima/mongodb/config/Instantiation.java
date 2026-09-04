a22dc01183356a359fa06815fb660b0a4d367067
unknown <leoaguiarlima@gmail.com>
2026-09-04 09:31:05 -0300

    Adding comments to posts

--- src/main/java/com/leandrolima/mongodb/config/Instantiation.java
+++ src/main/java/com/leandrolima/mongodb/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.leandrolima.mongodb.domain.Post;
 import com.leandrolima.mongodb.domain.User;
 import com.leandrolima.mongodb.dto.AuthorDTO;
+import com.leandrolima.mongodb.dto.CommentDTO;
 import com.leandrolima.mongodb.repository.PostRepository;
 import com.leandrolima.mongodb.repository.UserRepository;
 
@@ -39,6 +40,13 @@ public class Instantiation implements CommandLineRunner {
 		 
 		Post post1 = new Post(null, sdf.parse("21/08/2026"),"Novo projeto java", "Estou muito animado!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/08/2026"),"Projeto de Xadrez", "Adoro esse jogo!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Sucesso no projeto", sdf.parse("24/08/2026"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Se precisar de ajuda conte comigo", sdf.parse("25/08/2026"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("também adoro esse jogo depois, mande link do git", sdf.parse("26/08/2026"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		postRepository.saveAll(Arrays.asList(post1, post2));
